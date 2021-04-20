19a69f268c32c0fcd6b502b2b4a55b6e53d13136
danyg <danyg@JoseDany>
2021-04-20 12:04:17 -0500

    Aprendiando a programar 4S22

---
+++ src/g4s22p2jose8036/G4S22p2Jose8036.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p2jose8036;
+
+/**
+ *
+ * @author danyg
+ */
+public class G4S22p2Jose8036 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
